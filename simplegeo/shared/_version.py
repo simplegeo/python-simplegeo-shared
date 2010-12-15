85c18c1b95c0f9b6dc4fede7aa973aa3c0e9c196
Simple Geebus <nerds+geebus@simplegeo.com>
2010-12-15 23:36:22 +0000

    Prepared changelog for build 33

--- simplegeo/shared/_version.py
+++ simplegeo/shared/_version.py
@@ -1,6 +1,6 @@
 # This is the version of this source code.
 
-verstr = "2.0.32"
+verstr = "2.0.33"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
